412276b9e6633db28095dfc2cb789d746a33bb90
Yan Cong <52762906+yancong222@users.noreply.github.com>
2023-12-27 10:17:01 -0500

    Update discourse-connectives-LMs-Surprisals.py

--- scripts/discourse-connectives-LMs-Surprisals.py
+++ scripts/discourse-connectives-LMs-Surprisals.py
@@ -5,6 +5,10 @@ gpt2 = scorer.IncrementalLMScorer('gpt2', 'cuda')
 distilgpt2 = scorer.IncrementalLMScorer('distilgpt2', 'cuda')
 gptneo = scorer.IncrementalLMScorer('EleutherAI/gpt-neo-1.3B', 'cuda')
 
+gpt2Italian = scorer.IncrementalLMScorer('GroNLP/gpt2-small-italian', 'cuda')
+gpt2Chinese = scorer.IncrementalLMScorer('uer/gpt2-chinese-cluecorpussmall', 'cuda')
+gpt2French = scorer.IncrementalLMScorer('dbddv01/gpt2-french-small', 'cuda') 
+
 def target_surprisal(model, sentence, target):
   temp = []
   temp.append(sentence)
@@ -14,6 +18,37 @@ def target_surprisal(model, sentence, target):
   if len(result) == 0:
     result = [y[1] for x, y in enumerate(tuple_list) if y[0] in target]
   return round(np.nanmean(result), 2)
+
+def target_surprisal_sum(model, sentence, target):
+  temp = []
+  temp.append(sentence)
+  surp = model.token_score(temp, surprisal = True, base_two = True)
+  tuple_list = surp[0]
+  result = [y[1] for x, y in enumerate(tuple_list) if y[0] == target]
+  if len(result) == 1:
+    return round(np.nansum(result), 2)
+  else:
+    result = [y[1] for x, y in enumerate(tuple_list) if y[0] in target]
+    return round(np.nansum(result[:-1]), 2) 
+
+def get_tokens_len_score(sent, model):
+  input = []
+  input.append(sent)
+  output = model.token_score(input, surprisal = True, base_two = True)
+  result = []
+  return len(output[0])
+
+def target_oov(model, sentence, target):
+
+  temp = []
+  temp.append(sentence)
+  surp = model.token_score(temp, surprisal = True, base_two = True)
+  tuple_list = surp[0]
+  result = [y[1] for x, y in enumerate(tuple_list) if y[0] == target]
+  if len(result) == 1:
+    return 0
+  else:
+    return 1
 
 data = ('/your input folder/')
 output = ('/your output folder/')
